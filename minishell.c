28f9f4e8355a5d4060a68898462acb390601d004
Lenny dishane BACKORY <lebackor@e1r3p9.clusters.42paris.fr>
2022-05-20 15:09:46 +0200

    a

--- minishell.c
+++ minishell.c
@@ -7,8 +7,11 @@ int main(int ac, char **av, char **envp)
     (void) av;
     (void) envp;
     char *str;
+    int ret;
 
+    ret = kill(SIGINT)
     //printf( "%s\n", readline( ">$ " ) );
     str = readline(">$");
+    if ()
     printf("coco%s\n", str);
 }
